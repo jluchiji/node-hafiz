e6708757e3899f205b063468c143fadfcfa4baf7
Denis Luchkin-Zhou <denis@ricepo.com>
2016-01-04 15:55:20 -0500

    Tests for paths()

--- test/paths.spec.js
+++ test/paths.spec.js
@@ -4,3 +4,39 @@
  * @author  Denis Luchkin-Zhou <wyvernzora@gmail.com>
  * @license MIT
  */
+
+const paths = require('../lib/paths.js');
+
+
+describe('paths(base, more)', function() {
+
+  it('should split an PATH-like path string', function() {
+    const base = '/foo/bar:/hello/world:./test';
+    const actual = paths('/my/app', base);
+
+    expect(actual)
+      .to.deep.equal([
+        '/foo/bar',
+        '/hello/world',
+        '/my/app/test'
+      ]);
+  });
+
+  it('should append {more} to paths', function() {
+    const actual = paths('/my/app', null, [ './config', './test' ]);
+
+    expect(actual)
+      .to.deep.equal([
+        '/my/app/config',
+        '/my/app/test'
+      ]);
+  });
+
+  it('should generate empty array for empty base/more', function() {
+    const actual = paths('/my/app', null, null);
+
+    expect(actual)
+      .to.deep.equal([ ]);
+  });
+
+});
